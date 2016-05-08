aeaff5f80835212aadd9da74ce2acf65d590547d
juan267 <j.gomez267@gmail.com>
2016-05-08 18:20:56 -0500

    added conection to mongoDB

--- app.js
+++ app.js
@@ -11,4 +11,20 @@ app.set('view engine', 'html') // Only use nunjucks on file ending with html
 app.set('views', __dirname + '/views') // Where to find the view files
 app.use(bodyParser.urlenconded({extended: true})) // Url parser configuration
 
+mongoClient.connect('mongodb://localhost:27017/hacker-news', function(err, db){
+  assert.equal(null, err);
+  console.log('Succesfully connected to mongodb database')
 
+  app.use(errorHandler)
+
+  function errorHandler(err, req, res, next) {
+      console.error(err.message);
+      console.error(err.stack);
+      res.status(500).render('errorTemplate', { error: err });
+  }
+
+  var server = app.listen(3000, function(){
+    var port = server.address().port
+     console.log('Express server listening on port %s.', port);
+  })
+})
